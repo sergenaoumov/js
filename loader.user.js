b3569c981c997ed394cd77020143b12748e8a249
sergenaoumov <serge.naoumov@gmail.com>
2012-10-05 10:47:22 +0300

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -6,8 +6,3 @@
 // @include        http://*.travian.*
 // ==/UserScript==
 
-var script_link = 'http://www.jstoolbox.com/js/windows.js';
-var script_js = document.createElement('script');
-script_js.setAttribute('type', 'text/javascript');
-script_js.setAttribute('src', script_link);
-document.getElementsByTagName("head")[0].appendChild(script_js);
